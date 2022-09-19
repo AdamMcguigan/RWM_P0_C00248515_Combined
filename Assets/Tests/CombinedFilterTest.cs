89bf3b2db8318d558e463611bc676bef0508bfb2
AdamMcguigan <C00248515@itcarlow.ie>
2022-09-19 12:41:36 +0100

    Adding the Fact and the Dif filter together

---
+++ Assets/Tests/CombinedFilterTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+namespace Tests
+{
+    public class CombinedFilterTest
+    {
+        [Test]
+        public void CombinedFilterSimple()
+        {
+            int[] input = { 4, 2, 8, 3, 9, 4, 10, 5, 11, 6 };
+            int[] output = CombinedFilter.combinedFilter(input);
+            int[] expected = { 24, 2, 720, 120, 720, 120, 720, 120, 720, 120 };
+
+            CollectionAssert.AreEqual(expected, output);
+
+        }
+    }
+
+}
